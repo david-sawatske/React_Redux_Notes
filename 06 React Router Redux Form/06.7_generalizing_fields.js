23fd559fbb2336c0f76c72ac0b2ca4eca0f90507
David Sawatske <sawatskeda10@gmail.com>
2017-07-16 15:09:16 -0700

    Add notes for generalizing forms

---
+++ 06 React Router Redux Form/06.7_generalizing_fields.js
@@ -0,0 +1,48 @@
+// Field styling - job of the 'renderTitleField' function
+// DRY code by making general 'renderField' fn
+// - each '<Field' component uses different state
+// - have specific label for each by passing in properties to 'Field' component
+//   * those props will be passed into the 'field' arg to fn 'renderField(field)'
+
+// ./components/posts_new
+// ...
+class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">    // bootstrap styling
+        <label>{field.label}</label>  // title taken from prop on 'field' arg
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+// adding other Fields
+  render() {
+    return (
+      <form>
+        <Field
+          label="Title"                 // passing properties into the Field
+          name="title"                  // component, they are automatically
+          component={this.renderField}  // attached to 'field' arg for renderField
+        />
+        <Field
+          label="Tags"
+          name="tags"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
