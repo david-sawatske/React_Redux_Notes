e7faae540dd196962de2416fa214149127879ac6
David Sawatske <sawatskeda10@gmail.com>
2017-07-16 19:41:57 -0700

    Add notes for form validation and submittal'

--- 06 React Router Redux Form/06.7_generalizing_fields.js
+++ 06 React Router Redux Form/06.7_generalizing_fields.js
@@ -4,10 +4,11 @@
 // - have specific label for each by passing in properties to 'Field' component
 //   * those props will be passed into the 'field' arg to fn 'renderField(field)'
 
+
 // ./components/posts_new
 // ...
 class PostsNew extends Component {
-  renderField(field) {
+  renderField(field) {                // (field) represents a single piece of state
     return (
       <div className="form-group">    // bootstrap styling
         <label>{field.label}</label>  // title taken from prop on 'field' arg
@@ -28,7 +29,7 @@ class PostsNew extends Component {
           name="title"                  // component, they are automatically
           component={this.renderField}  // attached to 'field' arg for renderField
         />
-        <Field
+        <Field                          // new properties
           label="Tags"
           name="tags"
           component={this.renderField}
--- 06 React Router Redux Form/06.8_form_validation.js
+++ 06 React Router Redux Form/06.8_form_validation.js
@@ -10,8 +10,33 @@
 //          logic to validate inputs from 'values' object
 //          return errors object - if empty obj, there is nothing wrong
 
+
+// Displaying errors
+// - Redux Form: only responsible for managing state of form
+// - we need to show mark up on the screen (in <Form> comps) and errors, as well
+
+// - case: 'errors' object has a 'title' property, ReduxForm renders <Field>
+//   and passes along the error message into 'this.renderField' if the <Field>
+//   who's name matches the property of the errors obj.
+//   * the connection is 'errors.title' (validate fn) and name="title" (<Field>)
+
+// ./components/posts_new
 class PostsNew extends Component {
-// ...
+
+  renderField(field) {                // (field) represents a single piece of state
+    return (                          // with props, both defined and defaults
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input} // one of the above mentioned default properties
+        />
+        {field.meta.error} // '.meta.error' property is auto added to 'field'
+      </div>               // object from validate function below
+    );
+  }
+
 function validate(values) {
   const errors ={};
 
@@ -31,4 +56,44 @@ function validate(values) {
 export default reduxForm({
   validate,            // === 'validate: validate'   will be called on submition
   form: 'PostsNewForm'
+})(PostsNew);
+
+
+// Form Submittal - handle submittal with ReduxForm whenever HTML form is submitted
+// - define 'onSubmit={}' property on the form component, which is passed a fn
+//   * the passed in function will be called whenever the '<form>' is submitted
+
+// ReduxForm handles state, does not POST form data to server
+// we need create logic that takes the submitted vals and have them do what we want
+// 1. pull '{ handleSubmit }' property and pass to 'onSubmit={}'this.onSubmit.bind(this)
+//    - we will define 'this.onSubmit' helper fn
+
+//  onSubmit={handleSubmit(this.onSubmit.bind(this))}
+//  - 'handleSubmit' ReduxForm side that handles validation side (submit or not)
+//  - 'onSubmit' callback fn we define that recieves (values)
+//     * 'this.bind' because it is a callback fn that will called in a context
+//        outside of component in which it is defined ('this' still eq component)
+
+// still in ./components/posts_new
+class PostsNew extends Component {
+// ...
+  onSubmit(values) {
+    console.log(values);
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        // ...
+        <button type="submit" className="btn btn-primary">Submit</button> // new
+      </form>
+    );
+  }
+}
+// ...
+export default reduxForm({  // this reduxForm helper adds additional properties
+  validate,                 // that are passed to '(PostsNew)'
+  form: 'PostsNewForm'      // - including 'this.handleSubmit' above
 })(PostsNew);
